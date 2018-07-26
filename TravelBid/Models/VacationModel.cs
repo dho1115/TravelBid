01de5ce4f0e38d309cf5169fcf6c1ceb59363611
DAVID HO <davidhorph@hotmail.com>
2018-07-26 16:15:00 -0500

    Started 2nd project with TravelBid. Messed up the other one by changing the names.

---
+++ TravelBid/Models/VacationModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TravelBid.Models
+{
+    public class VacationModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string email { get; set; }
+        public double? phoneNumber { get; set; }
+        public string DreamDestination { get; set; }
+        public string DestinationDescription { get; set; }
+        public double budget { get; set; }
+    }
+}
