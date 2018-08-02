40f131407a021d1ff1e2dd157446bca1c0cec2a3
DAVID HO <davidhorph@hotmail.com>
2018-08-02 11:50:44 -0500

    Enter SqlCommand and  also assigned values to IDPosition, DestinationPositon, AttractionsPosition, etc....

--- TravelBid/Controllers/TravelResultsController.cs
+++ TravelBid/Controllers/TravelResultsController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -9,12 +10,54 @@ namespace TravelBid.Controllers
 {
     public class TravelResultsController : Controller
     {      
+        //SIDE NOTE: For now (8/2/18 @ 11:36am), we will ENTER the results via a SQL Query Server(new query) and then retrieve it via the "While" code below.
 
         public IActionResult Index(string BackToTravelResults)
        {
             //Notice below the data is "Hard coded" (Las Vegas, Comedy shows, etc...). What if I ran a SELECT... FROM. I need to open a connection, run a query and convert it.
 
             //GO TO NUGET.
+
+            string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=David;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+           System.Data.SqlClient.SqlConnection connection = new System.Data.SqlClient.SqlConnection(connectionString);
+
+           connection.Open();
+
+           connection.Close();
+
+            System.Data.SqlClient.SqlCommand command = connection.CreateCommand();
+
+            //command.CommandText = "Select top 1 where book id = " + ;
+
+            System.Data.SqlClient.SqlDataReader reader = command.ExecuteReader();
+
+            //BeachProduct model = new BeachProduct();
+
+            VacationSuggestions newdestination = new VacationSuggestions();
+
+            command.CommandText = "Select top 1 where book id = " + newdestination.ID;
+
+            //Names of the columns (idPosition, titlePosition, imagePathPosition, etc...) were all the Model property names taken from (in the example case) Beach product:
+
+            /*int idPosition = reader.GetOrdinal("ID");
+            int titlePosition = reader.GetOrdinal("Name");
+            int imagePathPosition = reader.getOrdinal("ImagePath");
+            int pricePosition = reader.*/
+
+            int IDPosition = reader.GetOrdinal("ID");
+            int DestinationPosition = reader.GetOrdinal("DestinationName");
+            int AttractionsPosition = reader.GetOrdinal("Attractions");
+            int BestTimeToGoPosition = reader.GetOrdinal("BestTimeToGo");                   
+            while (reader.Read())
+            {
+                //newdestination.Add();
+                newdestination.ID = reader.GetInt32(IDPosition);
+                newdestination.Attractions = reader.GetString(AttractionsPosition);
+                newdestination.BestTimeToGo = reader.GetString(BestTimeToGoPosition);
+            }//Will read data line to line
+
+            SqlCommand newCategoryCommand = connection.CreateCommand();
 
             List<VacationSuggestions> PlacesToGo = new List<VacationSuggestions>();            
 
@@ -24,6 +67,7 @@ namespace TravelBid.Controllers
 
                 PlacesToGo.Add(new VacationSuggestions
                 {
+                    ID = 1, //added this
                     DestinationName = "Las Vegas",
                     Attractions = "Cirque Du Soleil, Comedy shows, BUFFETS!!!",
                     BestTimeToGo = "Winter"                    
@@ -31,20 +75,23 @@ namespace TravelBid.Controllers
 
                 PlacesToGo.Add(new VacationSuggestions
                 {
+                    ID = 2,
                     DestinationName = "Reno",
                     Attractions = "Lake Tahoe, Virginia City, Skiing",
                     BestTimeToGo = "Fall"                  
                 });
 
                 PlacesToGo.Add(new VacationSuggestions
-                {                    
+                { 
+                    ID = 3,
                     DestinationName = "Costa Rica",
                     Attractions = "Cloud Forest, Nightlife, Beaches",
                     BestTimeToGo = "Mid December to April"
                 });
 
                 PlacesToGo.Add(new VacationSuggestions
-                {                    
+                { 
+                    ID = 4,
                     DestinationName = "Hong Kong",
                     Attractions = "Shopping, Symphony of light dinner cruise, Hong Kong Disneyland",
                     BestTimeToGo = "October to December",                    
@@ -52,6 +99,7 @@ namespace TravelBid.Controllers
 
                 PlacesToGo.Add(new VacationSuggestions
                 {
+                    ID = 5,
                     image = "~/ images /SouthBeach.jpg",
                     DestinationName = "South Beach",
                     Attractions = "FOOD!!!, Beach, Walk down Ocean Drive",
