473c8ef47498a246d6fd12df9fc9b909bea574e7
DAVID HO <davidhorph@hotmail.com>
2018-08-07 08:02:15 -0500

    ...

--- TravelBid/Data/ApplicationDbContext.cs
+++ TravelBid/Data/ApplicationDbContext.cs
@@ -12,13 +12,13 @@ namespace TravelBid.Data
     {
         public DbSet<VacationDetails> newvacationrequest { get; set; } //creates a new property and will be the name of the new table under ApplicationDBContext after you enter 'Add-Migration VacationDetails' in the Package Manager console.
 
-        public DbSet<TravelAgentParameters> TravelAgentParameters { get; set; }
+        public DbSet<TravelAgentParameters> TravelAgentParameters { get; set; }        
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
            
         }
-
+        
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
